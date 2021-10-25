d1a95712ce66ed175d6e03d708178f70d44e9830
Alexandra Negru <negru_alexandra90@yahoo.com>
2021-10-25 12:41:41 +0300

    Lab3

---
+++ Reconnect/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Reconnect
+//
+//  Created by Alexandra Negru on 25/10/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    let username: String = "ale"
+    let password: String = "parola"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        let username = usernameField.text
+        let password = passwordField.text
+        
+        if (username == username && password == password) {
+            performSegue(withIdentifier: "conversationsIdentifier", sender: self)
+        } else {
+            print("Incorrect user")
+        }
+    }
+    
+}
--- Reconnect/ViewController.swift
+++ Reconnect/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
 }
 
