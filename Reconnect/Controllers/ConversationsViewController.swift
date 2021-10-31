61298363bbc4288006adabeca991d661a01cb4c9
Alexandra Negru <negru_alexandra90@yahoo.com>
2021-10-31 14:28:41 +0200

    Add Firebase to project

---
+++ Reconnect/Controllers/ConversationsViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ConversationsViewController.swift
+//  Reconnect
+//
+//  Created by Alexandra Negru on 25/10/2021.
+//
+
+import UIKit
+
+class ConversationsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Convo"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
---
+++ Reconnect/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Reconnect
+//
+//  Created by Alexandra Negru on 30/10/2021.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
+
